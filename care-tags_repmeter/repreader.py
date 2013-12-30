32fc13e91db140ebf65cdacbf7a4f07479a92559
Ryan Bonick <rjbman93@gmail.com>
2013-12-30 10:30:58 -0600

    Altered RepReader to use local html file
    Due to the way the reputation system on care-tags.org works, you must be logged in to view a user's reputation. This means that the script would need to be logged in somehow (possible todo?).
    
    For now, I've changed it so that it reads everything from the local file "rep.html". Still need to finish parsing it correctly.

--- care-tags_repmeter/repreader.py
+++ care-tags_repmeter/repreader.py
@@ -9,24 +9,31 @@ class RepReader:
         self.usernumber = usernumber
 
     def parsehtml(self):
-        #Get access to user's rep page, and store it as Beautiful Soup  
-        #TODO: Figure out how to be able to access the reputation page
-        page = BeautifulSoup(urlopen("http://care-tags.org/reputation.php?&mode=details&u=" + str(self.usernumber)))
-         
-        #Find the overall number of reputation a user has
-        totalrep=self.__gettotalrep(page.find(id="post-reputation-list"))
-        
-        #Iterate through all the pages
-        repcount = 0 
-        while repcount < totalrep:
-            currentpage = BeautifulSoup(urlopen("http://care-tags.org/reputation.php?&mode=details&u=" + str(self.usernumber)+ "&start=" + repcount))
-           
-            #Parse each page
-            
-            
-            repcount = repcount + 15
+       # #Get access to user's rep page, and store it as Beautiful Soup  
+       # #TODO: Figure out how to be able to access the reputation page
+       # page = BeautifulSoup(urlopen("http://care-tags.org/reputation.php?&mode=details&u=" + str(self.usernumber)))
+       #  
+       # #Find the overall number of reputation a user has
+       # totalrep=self.__gettotalrep(page.find(id="post-reputation-list"))
+       # 
+       # #Iterate through all the pages
+       # repcount = 0 
+       # while repcount < totalrep:
+       #     currentpage = BeautifulSoup(urlopen("http://care-tags.org/reputation.php?&mode=details&u=" + str(self.usernumber)+ "&start=" + repcount))
+       #    
+       #     #Parse each page
+       #     
+       #     
+       #     repcount = repcount + 15
+       #
 
-        return true
+        # Open the file
+        soup = BeautifulSoup(open("rep.html"))
+
+        # Find the reputation list section
+        repsection = soup.find(id="post-reputation-list");
+
+        return True
 
     def __gettotalrep(self, tag):
         print("called __gettotalrep")

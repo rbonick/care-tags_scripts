232b9b66c09ca5c6ee0c6bf31d27012e744b994c
Ryan Bonick <rjbman93@gmail.com>
2014-03-01 18:52:37 -0600

    wrote some script - doesn't work

--- open_unread/open_unread.js
+++ open_unread/open_unread.js
@@ -1,3 +1,12 @@
-javascript: function(){
-	
-}
+javascript: (
+	function(){
+		var elements = document.getElementsByTagName("a");
+		for (var i=0; i<elements.length; ++i){
+			var currelem = elements[i];
+			if(currelem.href.indexOf("unread") != -1){
+				window.open(currelem.href, "_blank");
+			}
+		}
+	}
+)
+();

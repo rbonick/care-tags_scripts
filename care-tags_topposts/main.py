4265d2e03ae9834f166d9dccecbaf4d1adc739b2
Ryan <rjbman93@gmail.com>
2014-04-25 18:56:50 -0500

    Reworked main to have two writing functions instead.

--- care-tags_topposts/main.py
+++ care-tags_topposts/main.py
@@ -2,6 +2,27 @@ from topmembers import TopMembers
 from topposts import TopPosts
 import time
 
+
+def posts_to_bbcode(title, head1, head2, posts):
+    print "[size=200]", title, "[/size]"
+    print "[spoiler][b]", head1, "-",  head2, "[/b]"
+    print "[list=1]"
+    for post in posts:
+        print "[*][url=" + post[0] + "]" + "By " + post[1] + " - [b]" + str(post[2]) + "[/b]" + "[/url][/*]"
+    print "[/list][/spoiler]"
+    print " "
+
+
+def members_to_bbcode(title, head1, head2, members):
+    print "[size=200]", title, "[/size]"
+    print "[spoiler][b]", head1, "-",  head2, "[/b]"
+    print "[list=1]"
+    for member in members:
+        print "[*]" + member[0] + " - [b]" + str(member[1]) + "[/b]" + "[/*]"
+    print "[/list][/spoiler]"
+    print " "
+
+
 starttime = time.time()
 
 topmembers = TopMembers("PythonBot", "autonomous")
@@ -13,87 +34,14 @@ rep_per = topmembers.most_rep_per_post_users(10)
 posts = topposts.gettopposts(10)
 waywts = topposts.gettopwaywt(10)
 
+posts_to_bbcode("Top 10 Posts (non-WAYWT)", "Poster", "Reputation", posts)
 
-print "[size=200]Top 10 Posts (non-WAYWT)[/size]"
-print "[spoiler][b]" + "Poster" + " - Reputation[/b]"
-print "[list=1]"
-for post in posts:
-    print "[*][url=" + post[0] + "]" + "By " + post[1] + " - [b]" + str(post[2]) + "[/b]" + "[/url][/*]"
-print "[/list][/spoiler]"
-print " "
+posts_to_bbcode("Top 10 WAYWT Posts", "Poster", "Reputation", waywts)
 
-print "[size=200]Top 10 WAYWT Posts[/size]"
-print "[spoiler][b]" + "Poster" + " - Reputation[/b]"
-print "[list=1]"
-for post in waywts:
-    print "[*][url=" + post[0] + "]" + "By " + post[1] + " - [b]" + str(post[2]) + "[/b]" + "[/url][/*]"
-print "[/list][/spoiler]"
-print " "
+members_to_bbcode("Top 10 Members (post count)", "User", "Post Count", posters)
 
-print "[size=200]Top 10 Members (post count)[/size]"
-print "[spoiler][b]" + "User" + " - Post Count[/b]"
-print "[list=1]"
-for user in posters:
-    print "[*]" + user[0] + " - [b]" + str(user[1]) + "[/b]" + "[/*]"
-print "[/list][/spoiler]"
-print " "
+members_to_bbcode("Top 10 Members (reputation)", "User", "Reputation", repped)
 
-print "[size=200]Top 10 Members (reputation)[/size]"
-print "[spoiler][b]" + "User" + " - Reputation[/b]"
-print "[list=1]"
-for user in repped:
-    print "[*]" + user[0] + " - [b]" + str(user[1]) + "[/b]" + "[/*]"
-print "[/list][/spoiler]"
-print " "
-
-print "[size=200]Top 10 Members (rep per post)[/size]"
-print "[spoiler][b]" + "User" + " - Reputation Per Post[/b]"
-print "[list=1]"
-for user in rep_per:
-    print "[*]" + user[0] + " - [b]" + "{0:.2f}".format(user[1]) + "[/b]" + "[/*]"
-print "[/list][/spoiler]"
+members_to_bbcode("Top 10 Members (rep per post)", "User", "Reputation Per Post", rep_per)
 
 print time.time() - starttime  #
-
-
-# Desired output:
-# [size=200]Top 10 Members (post count)[/size]
-# [list=1][*]bela 1427[/*]
-# [*]cameron- 1382[/*]
-# [*]Syeknom 1254[/*]
-# [*]germinal 838[/*]
-# [*]starfox64 774[/*]
-# [*]sknss 666[/*]
-# [*]ramseames 511[/*]
-# [*]RycePooding 478[/*]
-# [*]smiles 434[/*]
-# [*]Bobbin.Threadbare 416[/*]
-# [/list]
-#
-# [size=200]Top 10 Members (reputation)[/size]
-# [list=1]
-# [*]cameron- 2352[/*]
-# [*]bela 1711[/*]
-# [*]Syeknom 1646[/*]
-# [*]germinal 1427[/*]
-# [*]smiles 1271[/*]
-# [*]sknss 1151[/*]
-# [*]Bobbin.Threadbare 1138[/*]
-# [*]maj 994[/*]
-# [*]odradek 943[/*]
-# [*]kyung 938[/*]
-# [/list]
-#
-# [size=200]Top 10 Posts (reputation)[/size]
-# [list=1]
-# [*](u'http://care-tags.org/viewtopic.php?f=2&t=8&start=1320#p16270', u'matrimonioids', 61)[/*]
-# [*](u'http://care-tags.org/viewtopic.php?f=3&t=413&start=0#p15225', u'odradek', 57)[/*]
-# [*](u'http://care-tags.org/viewtopic.php?f=2&t=8&start=1080#p13654', u'hmwut', 56)[/*]
-# [*](u'http://care-tags.org/viewtopic.php?f=3&t=413&start=0#p14883', u'maj', 56)[/*]
-# [*](u'http://care-tags.org/viewtopic.php?f=2&t=8&start=1020#p13251', u'agvs', 55)[/*]
-# [*](u'http://care-tags.org/viewtopic.php?f=2&t=8&start=1230#p15156', u'Rosenrot', 55)[/*]
-# [*](u'http://care-tags.org/viewtopic.php?f=3&t=413&start=30#p15472', u'verilyvert', 54)[/*]
-# [*](u'http://care-tags.org/viewtopic.php?f=2&t=8&start=1440#p18102', u'Rosenrot', 53)[/*]
-# [*](u'http://care-tags.org/viewtopic.php?f=2&t=8&start=1260#p15588', u'Bobbin.Threadbare', 52)[/*]
-# [*](u'http://care-tags.org/viewtopic.php?f=3&t=413&start=30#p15533', u'prawnzee', 51)[/*]
-# [/list]

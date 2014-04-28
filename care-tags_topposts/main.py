be805a73c1fc576e8f31ebaecc13b17f6e0d8312
Ryan <rjbman93@gmail.com>
2014-04-27 22:33:10 -0500

    TopMembers now returns an arbitrary number of users

--- care-tags_topposts/main.py
+++ care-tags_topposts/main.py
@@ -3,24 +3,24 @@ from topposts import TopPosts
 import time
 
 
-def posts_to_bbcode(title, head1, head2, posts):
-    print "[size=200]", title, "[/size]"
-    print "[spoiler][b]", head1, "-",  head2, "[/b]"
-    print "[list=1]"
+def posts_to_bbcode(title, head1, head2, posts, output):
+    output.write("[size=200]", title, "[/size]")
+    output.write("[spoiler][b]", head1, "-",  head2, "[/b]")
+    output.write("[list=1]")
     for post in posts:
-        print "[*][url=" + post[0] + "]" + "By " + post[1] + " - [b]" + str(post[2]) + "[/b]" + "[/url][/*]"
-    print "[/list][/spoiler]"
-    print " "
+        output.write("[*][url=" + post[0] + "]" + "By " + post[1] + " - [b]" + str(post[2]) + "[/b]" + "[/url][/*]")
+    output.write("[/list][/spoiler]")
+    output.write(" ")
 
 
-def members_to_bbcode(title, head1, head2, members):
-    print "[size=200]", title, "[/size]"
-    print "[spoiler][b]", head1, "-",  head2, "[/b]"
-    print "[list=1]"
+def members_to_bbcode(title, head1, head2, members, output):
+    output.write("[size=200]", title, "[/size]")
+    output.write("[spoiler][b]", head1, "-",  head2, "[/b]")
+    output.write("[list=1]")
     for member in members:
-        print "[*]" + member[0] + " - [b]" + str(member[1]) + "[/b]" + "[/*]"
-    print "[/list][/spoiler]"
-    print " "
+        output.write("[*]" + member[0] + " - [b]" + str(member[1]) + "[/b]" + "[/*]")
+    output.write("[/list][/spoiler]")
+    output.write(" ")
 
 
 starttime = time.time()
@@ -28,20 +28,18 @@ starttime = time.time()
 topmembers = TopMembers("PythonBot", "autonomous")
 topposts = TopPosts("PythonBot", "autonomous")
 
-repped = topmembers.mostreppedusers()
-posters = topmembers.gettopposters()
+repped = topmembers.mostreppedusers(10)
+posters = topmembers.gettopposters(10)
 rep_per = topmembers.most_rep_per_post_users(10)
 posts = topposts.gettopposts(10)
 waywts = topposts.gettopwaywt(10)
 
-posts_to_bbcode("Top 10 Posts (non-WAYWT)", "Poster", "Reputation", posts)
+with open("output.txt", "a") as f:
+    f.write("Current date & time " + time.strftime("%c"))
+    posts_to_bbcode("Top 10 Posts (non-WAYWT)", "Poster", "Reputation", posts, f)
+    posts_to_bbcode("Top 10 WAYWT Posts", "Poster", "Reputation", waywts, f)
+    members_to_bbcode("Top 10 Members (post count)", "User", "Post Count", posters, f)
+    members_to_bbcode("Top 10 Members (reputation)", "User", "Reputation", repped, f)
+    members_to_bbcode("Top 10 Members (rep per post)", "User", "Reputation Per Post", rep_per, f)
 
-posts_to_bbcode("Top 10 WAYWT Posts", "Poster", "Reputation", waywts)
-
-members_to_bbcode("Top 10 Members (post count)", "User", "Post Count", posters)
-
-members_to_bbcode("Top 10 Members (reputation)", "User", "Reputation", repped)
-
-members_to_bbcode("Top 10 Members (rep per post)", "User", "Reputation Per Post", rep_per)
-
-print time.time() - starttime  #
+    f.write(time.time() - starttime)  #
--- care-tags_topposts/topmembers.py
+++ care-tags_topposts/topmembers.py
@@ -12,45 +12,66 @@ class TopMembers:
         self.session = requests.Session()
 
     # Returns list containing top 10 users (sorted by post count) and their post count as a tuple
-    def gettopposters(self):
+    def gettopposters(self, return_count):
         soup = self.__getbs("http://care-tags.org/memberlist.php?mode=&sk=d&sd=d#memberlist")
         postcountlist = []
 
-        memberlist = soup.find(id="memberlist").tbody
-        for member in memberlist.children:
-            if member.string is None:  # Gets rid of the newlines
-                # Store username and post count pair in the list
-                user = member.td.a.string
-                postcount = int(member.find(class_="posts").string)
-                postcountlist.append((user, postcount))
+        # Get number of members
+        num_members = int(soup.find(class_="pagination").text.split()[0])  # Gets the thread count
+        print num_members
+
+        curr_members = 0
+        while curr_members < num_members:
+            print "Working, page", curr_members/25 + 1
+            soup = self.__getbs("http://care-tags.org/memberlist.php?sk=d&sd=d&start=" + str(curr_members))
+
+            memberlist = soup.find(id="memberlist").tbody
+            for member in memberlist.children:
+                if member.string is None:  # Gets rid of the newlines
+                    # Store username and post count pair in the list
+                    user = member.td.a.string
+                    postcount = int(member.find(class_="posts").string)
+                    postcountlist.append((user, postcount))
+            curr_members += 25
 
         # Take the top 10 users
-        return postcountlist[:10]
+        return postcountlist[:return_count]
 
     # Returns list containing top 10 users (sorted by reputation) and their reputation as a tuple
-    def mostreppedusers(self):
+    def mostreppedusers(self, return_count):
         soup = self.__getbs("http://care-tags.org/memberlist.php?mode=&sk=r&sd=d#memberlist")
         userreplist = []
 
-        memberlist = soup.find(id="memberlist").tbody
-        for member in memberlist.children:
-            if member.string is None:  # Get rid of newlines
-                # print member.prettify()
-                user = member.td.a.string
-                repcount = member.find_all(class_="posts")[1].string
-                userreplist.append((user, repcount))
-        # Take top 10 users
-        return userreplist[:10]
+        # Get number of members
+        num_members = int(soup.find(class_="pagination").text.split()[0])  # Gets the thread count
+        print num_members
 
-    # Returns list containing top NUM users (sorted by rep per post count) in the form
+        curr_members = 0
+        while curr_members < num_members:
+            print "Working, page", curr_members/25 + 1
+            soup = self.__getbs("http://care-tags.org/memberlist.php?sk=r&sd=d&start=" + str(curr_members))
+
+            memberlist = soup.find(id="memberlist").tbody
+            for member in memberlist.children:
+                if member.string is None:  # Get rid of newlines
+                    # print member.prettify()
+                    user = member.td.a.string
+                    repcount = member.find_all(class_="posts")[1].string
+                    userreplist.append((user, repcount))
+            curr_members += 25
+
+        # Take top 10 users
+        return userreplist[:return_count]
+
+    # Returns list containing top return_count users (sorted by rep per post count) in the form
     # (User, rep/post, posts, rep)
-    def most_rep_per_post_users(self, num):
+    def most_rep_per_post_users(self, return_count):
         users = []
         soup = self.__getbs("http://care-tags.org/memberlist.php")
         num_users = int(soup.find(class_ = "rightside pagination").text.split()[0])
         curr_user = 0
         while curr_user < num_users:
-            print "Working..."
+            print "Working, page", curr_user/25 + 1
             soup = self.__getbs("http://care-tags.org/memberlist.php?start=" + str(curr_user))
             body = soup.tbody
             for child in body.children:
@@ -65,8 +86,7 @@ class TopMembers:
                     users.append((user, rep_per, posts, rep))
             curr_user += 25
         users.sort(key=itemgetter(1), reverse=True)
-        return users[:num]
-
+        return users[:return_count]
 
     def __getbs(self, url):
         # Initial variables
@@ -126,14 +146,14 @@ if __name__ == "__main__":
     # (options, args) = parser.parse_args()
 
     hakuna = TopMembers("pythonbot", "autonomous")
-    #print "Top posters by post count:"
-    #for poster in hakuna.gettopposters():
-        #print str(poster[0]) + " " + str(poster[1])
+    print "Top posters by post count:"
+    for poster in hakuna.gettopposters(10):
+        print poster[0], poster[1]
 
-    #print "Top posters by reputation:"
-    #for poster in hakuna.mostreppedusers():
-        #print str(poster[0]) + " " + str(poster[1])
+    print "Top posters by reputation:"
+    for poster in hakuna.mostreppedusers(10):
+        print poster[0], poster[1]
 
     print "Top repped per post:"
     for poster in hakuna.most_rep_per_post_users(10):
-        print poster
+        print poster[0], poster[1]
